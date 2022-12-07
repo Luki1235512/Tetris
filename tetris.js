39f962b19b866c501a050b57a352b4d873ba3953
Luki1235512 <Luki1235512@gmail.com>
2022-12-07 09:38:31 +0100

    Initial commit

---
+++ tetris.js
@@ -0,0 +1,36 @@
+class Tetris {
+    constructor(imageX, imageY, template) {
+
+        this.imageX = imageX;
+        this.imageY = imageY;
+        this.template = template;
+    }
+
+    checkBottom() {
+
+    }
+
+    checkLeft() {
+
+    }
+
+    checkRight() {
+
+    }
+
+    moveRight() {
+
+    }
+
+    moveLeft() {
+
+    }
+
+    moveBottom() {
+
+    }
+
+    changeRotation() {
+
+    }
+}
